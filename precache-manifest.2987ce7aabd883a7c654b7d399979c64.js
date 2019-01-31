d4482f9d347b32573e0397fdc361dcc68f9b7823
gitmatheo <matteuszd@gmail.com>
2019-01-31 19:30:22 +0100

    Updates

---
+++ precache-manifest.2987ce7aabd883a7c654b7d399979c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa19a6578a8f5ba7892a93385eab3ab",
+    "url": "/React-todoapp/static/media/bg.1aa19a65.jpg"
+  },
+  {
+    "revision": "cd5bb108ec317c220f0df789b562922a",
+    "url": "/React-todoapp/static/media/avatar.cd5bb108.png"
+  },
+  {
+    "revision": "0bae50224e6d364d9bea",
+    "url": "/React-todoapp/static/js/runtime~main.0bae5022.js"
+  },
+  {
+    "revision": "7ca634019f14a58f0099",
+    "url": "/React-todoapp/static/js/main.7ca63401.chunk.js"
+  },
+  {
+    "revision": "235d115175e007320362",
+    "url": "/React-todoapp/static/js/1.235d1151.chunk.js"
+  },
+  {
+    "revision": "7ca634019f14a58f0099",
+    "url": "/React-todoapp/static/css/main.ed5e4e2b.chunk.css"
+  },
+  {
+    "revision": "485b32983da67feacd6785b001a1fe42",
+    "url": "/React-todoapp/index.html"
+  }
+];
